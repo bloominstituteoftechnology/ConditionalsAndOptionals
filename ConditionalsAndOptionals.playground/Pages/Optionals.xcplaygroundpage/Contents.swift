b7b737f08eef76f5a98d0200afb4c02ec393bad8
Andrew Madsen <andrew@openreelsoftware.com>
2018-06-13 21:15:47 -0600

    Minor tweaks

--- ConditionalsAndOptionals.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ ConditionalsAndOptionals.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@
  
  Imagine you have an app that asks the user to enter his/her age using the keyboard. When your app allows a user the input text, what is captured for you is given as a `String`. However, you want to store this information as an `Int`. Is it possible for the user to make a mistake and for the input to not match the type you want to store?
  
- Declare a constant `userInputAge` of type `String` and assign it "34e" to simulate a typo while typing age. Then declare a constant `userAge` of type `Int` and use the `Int` initializer and pass in `userInputAge`. What error do you get?
+ Declare a constant `userInputAge` of type `String` and assign it "34e" to simulate a typo while typing age. Then declare a constant `userAge` of type `Int` and use the `Int` initializer and pass in `userInputAge` (`let userAge = Int(userInputAge)`). What error do you get?
  */
 
 
@@ -17,11 +17,6 @@
 /*:
  Now go back and fix the typo on the value of `userInputAge`. Is there anything about the value printed that seems off?
  
- Print `userAge` again, but this time unwrap `userAge` using the bang operator.
- */
-
-
-/*:
  Now use optional binding to unwrap `userAge`. If `userAge` has a value, print it to the console.
  */
 
