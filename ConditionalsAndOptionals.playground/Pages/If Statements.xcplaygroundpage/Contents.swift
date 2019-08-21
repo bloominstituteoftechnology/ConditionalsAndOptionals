f83b95bd0a37794a5ab2526467643a43145763d2
Spencer Curtis <spencercolecurtis@gmail.com>
2019-08-21 16:00:26 -0600

    Clean up playground instructions' formatting

--- ConditionalsAndOptionals.playground/Pages/If Statements.xcplaygroundpage/Contents.swift
+++ ConditionalsAndOptionals.playground/Pages/If Statements.xcplaygroundpage/Contents.swift
@@ -7,13 +7,13 @@ var dollars = 0
 
 
 /*:
- `dollars` has been updated below to have a value of 10. Write an an if-else statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, but prints "You've got some spending money!" otherwise. Observe what is printed to the console.
+ `dollars` has been updated below to have a value of 10. Write a new if-else statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, but prints "You've got some spending money!" otherwise. Observe what is printed to the console.
  */
 dollars = 10
 
 
 /*:
- Again, `dollars` has been updated below to have a value of 105. Write an an if-else-if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, prints "You've got some spending money!" if `dollars` is less than 100, and prints "Looks to me like you're rich!" otherwise. Observe what is printed to the console.
+ Again, `dollars` has been updated below to have a value of 105. Write a new if-else-if statement that prints "Sorry, kid. You're broke!" if `dollars` has a value of 0, prints "You've got some spending money!" if `dollars` is less than 100, and prints "Looks to me like you're rich!" otherwise. Observe what is printed to the console.
  */
 dollars = 105
 
